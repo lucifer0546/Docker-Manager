840c748ac83eff3728f89a1c25f1bc9853fab0c5
lucifer0546 <blackgear@gmail.com>
2023-12-29 12:02:46 +0100

    docker containers redone

--- docker.sh
+++ docker.sh
@@ -1,193 +1,10 @@
 #!/bin/bash
 
-function choose_docker_container() {
-    containers=$(sudo docker ps -a --format "{{.ID}}\t{{.Names}}")
-
-    if [[ -z $containers ]]; then
-        echo "No Docker containers found."
-        return
-    fi
-
-    echo "Available Docker containers:"
-    echo "$containers"
-
-    read -p "Enter the number of the container you want to choose: " choice
-
-    selected_container=$(echo "$containers" | awk -v choice="$choice" 'NR==choice {print $1}')
-
-    if [[ -z $selected_container ]]; then
-        echo "Invalid choice. Please try again."
-        return
-    fi
-
-    echo "Selected container: $selected_container"
-}
-
-function docker_start() {
-    choose_docker_container
-
-    if [[ -z $selected_container ]]; then
-        return
-    fi
-
-    sudo docker start "$selected_container"
-}
-
-function docker_stop() {
-    choose_docker_container
-
-    if [[ -z $selected_container ]]; then
-        return
-    fi
-
-    sudo docker stop "$selected_container"
-}
-
-function docker_restart() {
-    choose_docker_container
-
-    if [[ -z $selected_container ]]; then
-        return
-    fi
-
-    sudo docker restart "$selected_container"
-}
-
-function docker_shell() {
-    choose_docker_container
-
-    if [[ -z $selected_container ]]; then
-        return
-    fi
-
-    sudo docker exec -it "$selected_container" /bin/bash
-}
-
-function docker_remove() {
-    choose_docker_container
-
-    if [[ -z $selected_container ]]; then
-        return
-    fi
-
-    sudo docker rm "$selected_container"
-}
-
-function docker_remove_all() {
-    containers=$(sudo docker ps -a --format "{{.ID}}\t{{.Names}}")
-
-    if [[ -z $containers ]]; then
-        echo "No Docker containers found."
-        return
-    fi
-
-    echo "Available Docker containers:"
-    awk '{print NR".", $0}' <<< "$containers"
-
-    read -p "Are you sure you want to remove all containers? [y/N] " choice
-
-    if [[ $choice != "y" ]]; then
-        echo "Aborting."
-        return
-    fi
-
-    sudo docker rm $(echo "$containers" | awk '{print $1}')
-}
-
-
-function docker_image_remove() {
-    images=$(sudo docker images --format "{{.ID}}\t{{.Repository}}\t{{.Tag}}")
-
-    if [[ -z $images ]]; then
-        echo "No Docker images found."
-        return
-    fi
-
-    echo "Available Docker images:"
-    awk '{print NR".", $0}' <<< "$images"
-
-    read -p "Enter the number of the image you want to remove: " choice
-
-    selected_image=$(echo "$images" | awk -v choice="$choice" 'NR==choice {print $1}')
-
-    if [[ -z $selected_image ]]; then
-        echo "Invalid choice. Please try again."
-        return
-    fi
-
-    echo "Selected image: $selected_image"
-
-    sudo docker rmi "$selected_image"
-}
-
-function docker_image_remove_all() {
-    images=$(sudo docker images --format "{{.ID}}\t{{.Repository}}\t{{.Tag}}")
-
-    if [[ -z $images ]]; then
-        echo "No Docker images found."
-        return
-    fi
-
-    echo "Available Docker images:"
-    echo "$images"
-
-    read -p "Are you sure you want to remove all images? [y/N] " choice
-
-    if [[ $choice != "y" ]]; then
-        echo "Aborting."
-        return
-    fi
-
-    sudo docker rmi $(echo "$images" | awk '{print $1}')
-}
-
-function docker_image_remove_dangling() {
-    images=$(sudo docker images --format "{{.ID}}\t{{.Repository}}\t{{.Tag}}")
-
-    if [[ -z $images ]]; then
-        echo "No Docker images found."
-        return
-    fi
-
-    echo "Available Docker images:"
-    echo "$images"
-
-    read -p "Are you sure you want to remove all dangling images? [y/N] " choice
-
-    if [[ $choice != "y" ]]; then
-        echo "Aborting."
-        return
-    fi
-
-    sudo docker rmi $(sudo docker images -f "dangling=true" -q)
-}
-
-function docker_image_remove_all_except_latest() {
-    images=$(sudo docker images --format "{{.ID}}\t{{.Repository}}\t{{.Tag}}")
-
-    if [[ -z $images ]]; then
-        echo "No Docker images found."
-        return
-    fi
-
-    echo "Available Docker images:"
-    echo "$images"
-
-    read -p "Are you sure you want to remove all images except the latest? [y/N] " choice
-
-    if [[ $choice != "y" ]]; then
-        echo "Aborting."
-        return
-    fi
-
-    sudo docker rmi $(echo "$images" | awk '$3 != "latest" {print $1}')
-}
-
 function main_menu() {
     clear
     echo "Select an option:"
-    echo "1. Manage Docker Images"
-    echo "2. Manage Docker Containers"
+    echo "1. Manage Docker Containers"
+    echo "2. Manage Docker Images"
     echo "3. Execute Docker Command"
     echo "4. Exit"
 
@@ -195,15 +12,12 @@ function main_menu() {
 
     case $choice in
         1)
-            docker_image_menu
-            ;;
-        2)
             docker_container_menu
             ;;
-        3)
-            execute_docker_command
+        2)
+            docker_image_menu
             ;;
-        4)
+        3)
             echo "Exiting..."
             return
             ;;
@@ -214,6 +28,126 @@ function main_menu() {
     esac
 }
 
+function docker_container_menu() {
+    clear
+    echo "Docker Container Menu:"
+    echo "1. Start a Docker Container"
+    echo "2. Stop a Docker Container"
+    echo "3. Restart a Docker Container"
+    echo "4. Open a Shell in a Docker Container"
+    echo "5. Remove a Docker Container"
+    echo "6. Remove all Docker Containers"
+    echo "7. Back to Main Menu"
+
+    read -p "Enter your choice: " choice
+
+    case $choice in
+        1)
+            docker_start
+            ;;
+        2)
+            docker_stop
+            ;;
+        3)
+            docker_restart
+            ;;
+        4)
+            docker_shell
+            ;;
+        5)
+            docker_container_remove
+            ;;
+        6)
+            docker_remove_all
+            ;;
+        7)
+            main_menu
+            ;;
+        *)
+            echo "Invalid choice. Please try again."
+            docker_container_menu
+            ;;
+    esac
+}
+
+function docker_start() {
+    # Print all Docker containers with a number for ID
+    docker ps -a --format "table {{.ID}}\t{{.Names}}"
+
+    # Prompt user to enter the number of the Docker container to start
+    read -p "Enter the number of the Docker container to start: " container_number
+
+    # Start the Docker container based on the selected number
+    docker start $(docker ps -a --format "{{.ID}}" | sed -n "${container_number}p")
+
+    echo "Starting a Docker container..."
+}
+
+function docker_stop() {
+    # Print all Docker containers with a number for ID
+    docker ps -a --format "table {{.ID}}\t{{.Names}}"
+
+    # Prompt user to enter the number of the Docker container to stop
+    read -p "Enter the number of the Docker container to stop: " container_number
+
+    # Stop the Docker container based on the selected number
+    docker stop $(docker ps -a --format "{{.ID}}" | sed -n "${container_number}p")
+
+    echo "Stopping a Docker container..."
+}
+
+function docker_restart() {
+    # Print all Docker containers with a number for ID
+    docker ps -a --format "table {{.ID}}\t{{.Names}}"
+
+    # Prompt user to enter the number of the Docker container to restart
+    read -p "Enter the number of the Docker container to restart: " container_number
+
+    # Restart the Docker container based on the selected number
+    docker restart $(docker ps -a --format "{{.ID}}" | sed -n "${container_number}p")
+
+    echo "Restarting a Docker container..."
+}
+
+function docker_shell() {
+    # Print all Docker containers with a number for ID
+    docker ps -a --format "table {{.ID}}\t{{.Names}}"
+
+    # Prompt user to enter the number of the Docker container to open a shell
+    read -p "Enter the number of the Docker container to open a shell: " container_number
+
+    # Open a shell in the Docker container based on the selected number
+    docker exec -it $(docker ps -a --format "{{.ID}}" | sed -n "${container_number}p") /bin/bash
+
+    echo "Opening a shell in a Docker container..."
+}
+
+function docker_container_remove() {
+    # Print all Docker containers with a number for ID
+    docker ps -a --format "table {{.ID}}\t{{.Names}}"
+
+    # Prompt user to enter the number of the Docker container to remove
+    read -p "Enter the number of the Docker container to remove: " container_number
+
+    # Remove the Docker container based on the selected number
+    docker rm $(docker ps -a --format "{{.ID}}" | sed -n "${container_number}p")
+
+    echo "Removing a Docker container..."
+}
+
+function docker_remove_all() {
+    read -p "Are you sure you want to remove all Docker containers? Type 'yes' to confirm: " confirm
+
+    if [ "$confirm" == "yes" ]; then
+        # Remove all Docker containers
+        docker rm $(docker ps -a -q)
+
+        echo "Removing all Docker containers..."
+    else
+        echo "Operation cancelled."
+    fi
+}
+
 function docker_image_menu() {
     clear
     echo "Docker Image Menu:"
@@ -221,8 +155,7 @@ function docker_image_menu() {
     echo "2. Remove all Docker Images"
     echo "3. Remove all dangling Docker Images"
     echo "4. Remove all Docker Images except the latest"
-    echo "5. View Docker Image Info"
-    echo "6. Back to Main Menu"
+    echo "5. Back to Main Menu"
 
     read -p "Enter your choice: " choice
 
@@ -240,9 +173,6 @@ function docker_image_menu() {
             docker_image_remove_all_except_latest
             ;;
         5)
-            view_docker_image_info
-            ;;
-        6)
             main_menu
             ;;
         *)
@@ -252,90 +182,27 @@ function docker_image_menu() {
     esac
 }
 
-function docker_container_menu() {
-    clear
-    echo "Docker Container Menu:"
-    echo "1. Remove a Docker Container"
-    echo "2. View Docker Container Info"
-    echo "3. Back to Main Menu"
+function check_docker_status() {
+    # Check if Docker is running
+    if ! systemctl is-active --quiet docker; then
+        read -p "Docker is not running. Would you like to start it? (yes/no): " start_docker
 
-    read -p "Enter your choice: " choice
+        if [ "$start_docker" == "yes" ]; then
+            # Check if user has sudo permission
+            if [ "$(id -u)" != "0" ]; then
+                echo "You don't have sudo permission. Please run the script as root or with sudo."
+                exit 1
+            fi
 
-    case $choice in
-        1)
-            docker_container_remove
-            ;;
-        2)
-            view_docker_container_info
-            ;;
-        3)
-            main_menu
-            ;;
-        *)
-            echo "Invalid choice. Please try again."
-            docker_container_menu
-            ;;
-    esac
+            # Start Docker
+            systemctl start docker
+            echo "Docker has been started."
+        else
+            echo "Docker is not running. Exiting..."
+            exit 1
+        fi
+    fi
 }
 
-function docker_container_remove() {
-    containers=$(sudo docker ps -a --format "{{.ID}}\t{{.Names}}")
-
-    if [[ -z $containers ]]; then
-        echo "No Docker containers found."
-        return
-    fi
-
-    echo "Available Docker containers:"
-    echo "$containers"
-
-    read -p "Enter the number of the container you want to remove: " choice
-
-    selected_container=$(echo "$containers" | awk -v choice="$choice" 'NR==choice {print $1}')
-
-    if [[ -z $selected_container ]]; then
-        echo "Invalid choice. Please try again."
-        return
-    fi
-
-    echo "Selected container: $selected_container"
-
-    sudo docker rm "$selected_container"
-}
-
-function view_docker_container_info() {
-    containers=$(sudo docker ps -a --format "{{.ID}}\t{{.Names}}\t{{.Image}}\t{{.Status}}")
-
-    if [[ -z $containers ]]; then
-        echo "No Docker containers found."
-        return
-    fi
-
-    echo "Available Docker containers:"
-    echo "$containers"
-}
-
-function view_docker_image_info() {
-    images=$(sudo docker images --format "{{.ID}}\t{{.Repository}}\t{{.Tag}}\t{{.Size}}")
-
-    if [[ -z $images ]]; then
-        echo "No Docker images found."
-        return
-    fi
-
-    echo "Available Docker images:"
-    echo "$images"
-}
-
-function execute_docker_command() {
-    read -p "Enter the Docker command to execute: " command
-
-    if [[ -z $command ]]; then
-        echo "Invalid command. Please try again."
-        return
-    fi
-
-    sudo docker $command
-}
-
+check_docker_status
 main_menu
